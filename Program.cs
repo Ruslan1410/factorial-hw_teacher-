fb0ea3480ba13fa87494a279920540821d2a4f54
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-15 20:24:08 +0300

    factorial wirh chatGPT

--- Program.cs
+++ Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace factorial
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Введите число для вычисления факториала: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number) && number >= 0)
         {
+            long factorial = CalculateFactorial(number);
+
+            Console.WriteLine($"Факториал числа {number} равен: {factorial}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите неотрицательное целое число.");
         }
     }
+
+    // Метод для вычисления факториала числа
+    static long CalculateFactorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+
+        long result = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
 }
+
+       
